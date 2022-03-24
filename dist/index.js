1913758636ad2524ff5198f8d95a44f8780af035
tetsunari <mtst.tty556@gmail.com>
2022-03-24 19:35:18 +0900

    first commit

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+let a = 30;
+let b = a === 30;
+let c = a + 20;
+if (typeof a === 'number') {
+    console.log(a + 10);
+}
+console.log(a);
+console.log(b);
+console.log(c);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+let a: unknown = 30;
+let b = a === 30;
+let c = a + 20;
+if (typeof a === 'number') {
+    console.log(a + 10);
+}
+console.log(a);
+console.log(b);
+console.log(c);
