631bf0b3ac96ef3d92bac0d9e918e0e1af5297e7
tetsunari <mtst.tty556@gmail.com>
2022-03-24 19:37:24 +0900

    add coment

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 let a: unknown = 30;
 let b = a === 30;
-let c = a + 20;
+let e = a === 20;
+let c = a + 20; // error
 if (typeof a === 'number') {
-    console.log(a + 10);
+    console.log(a + 10); //40
 }
 console.log(a);
-console.log(b);
+console.log(b); //true
+console.log(e); //false
 console.log(c);
