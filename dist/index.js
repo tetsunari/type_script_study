f738c0927e4c24862820e0caf9bff8aa423b8a21
tetsunari <mtst.tty556@gmail.com>
2022-03-24 19:59:52 +0900

    object

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,6 @@
 "use strict";
-let a = 1234;
-var b = Infinity * 0.10;
-const c = 5678;
-let d = a < b;
-let e = 100;
-let f = 26.1;
-let g = 10;
-console.log(a); // ture
-console.log(b); // false
-console.log(c); // true
-console.log(d); // true
-console.log(e); // true
-console.log(f); // error
-console.log(g); // undefined
+let a = {
+    b: 'x'
+};
+console.log(a.b);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,4 @@
-let a = 1234;
-var b = Infinity * 0.10;
-const c = 5678;
-let d = a < b;
-let e: number = 100;
-let f: 26.1 = 26.1;
-let g: 26.1 = 10;
-
-
-console.log(a); // 1234
-console.log(b); // Infinity
-console.log(c); // 5678
-console.log(d); // true
-console.log(e); // 100
-console.log(f); // 26.1
-console.log(g); // error 10
+let a: object = {
+    b: 'x'
+}
+console.log(a.b); // error x
