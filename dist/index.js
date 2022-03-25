686bd1d427d176a5279602ea4c32d79ac410c036
tetsunari <mtst.tty556@gmail.com>
2022-03-25 20:38:36 +0900

    readonly tuple_2

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,7 @@
 "use strict";
-let as = [1, 2, 3];
-let bs = as.concat(4);
-let cs = as.slice(1, 2);
-let three = bs[2];
-console.log(as); // [ 1, 2, 3 ]
-console.log(bs); // [ 1, 2, 3, 4 ]
-console.log(cs); // [ 2 ]
-console.log(three); // 3
-as[4] = 5;
-as.push(6);
+let a = ['test', 'aa'];
+let test = a.concat('aaa');
+console.log(test);
+let b = ['test', 'aa'];
+let d = [1, 'd'];
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,14 @@
-let as: readonly number[] = [1, 2, 3];
-let bs: readonly number[] = as.concat(4);
-let cs: readonly number[] = as.slice(1, 2);
-let three = bs[2];
+type A = readonly string[]; // readonly string[]
+type B = ReadonlyArray<string> // readonly string[]
+type C = Readonly<string> // readonly string[]
 
-console.log(as); // [ 1, 2, 3 ]
-console.log(bs); // [ 1, 2, 3, 4 ]
-console.log(cs); // [ 2 ]
-console.log(three); // 3
+type D = readonly [number, string] // readonly [number, string]
+type E = Readonly<[number, string]> // readonly [number, string]
 
-as[4] = 5;
-as.push(6);
+let a: A = ['test', 'aa'];
+let test = a.concat('aaa');
+console.log(test);
+let b: B = ['test', 'aa'];
+let d: D = [1, 'd'];
 
-// error
-// src/index.ts:11:1 - error TS2542: Index signature in type 'readonly number[]' only permits reading.
-//
-// 11 as[4] = 5;
-// ~~~~~
-//
-//     src/index.ts:12:4 - error TS2339: Property 'push' does not exist on type 'readonly number[]'.
-//
-// 12 as.push(6);
-// ~~~~
+// 上記は全て正常動作
