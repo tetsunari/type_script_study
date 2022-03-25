6df117c27a2fe7c3de022f38a956fae8e6db76a8
tetsunari <mtst.tty556@gmail.com>
2022-03-25 20:30:55 +0900

    readonly tuple

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,12 @@
 "use strict";
-let trainFares = [
-    // let trainFares: ([number] | [number, number][])ともかける
-    [2, 3],
-    [1.234],
-];
-let friends = ['sara', 'tali', 'chloe', 'claire'];
-// 少なくとも1つの要素
-let list = [1, false, 'a', 'b', 'c'];
-// 不均一なリスト
+let as = [1, 2, 3];
+let bs = as.concat(4);
+let cs = as.slice(1, 2);
+let three = bs[2];
+console.log(as); // [ 1, 2, 3 ]
+console.log(bs); // [ 1, 2, 3, 4 ]
+console.log(cs); // [ 2 ]
+console.log(three); // 3
+as[4] = 5;
+as.push(6);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,23 @@
-let trainFares: [number, number?][] = [
-    // let trainFares: ([number] | [number, number][])ともかける
-    [2, 3],
-    [1.234],
-]
+let as: readonly number[] = [1, 2, 3];
+let bs: readonly number[] = as.concat(4);
+let cs: readonly number[] = as.slice(1, 2);
+let three = bs[2];
 
-let friends: [string, ...string[]] = ['sara', 'tali', 'chloe', 'claire'];
-// 少なくとも1つの要素
+console.log(as); // [ 1, 2, 3 ]
+console.log(bs); // [ 1, 2, 3, 4 ]
+console.log(cs); // [ 2 ]
+console.log(three); // 3
 
-let list: [number, boolean, ...string[]] = [1, false, 'a', 'b', 'c'];
-// 不均一なリスト
+as[4] = 5;
+as.push(6);
+
+// error
+// src/index.ts:11:1 - error TS2542: Index signature in type 'readonly number[]' only permits reading.
+//
+// 11 as[4] = 5;
+// ~~~~~
+//
+//     src/index.ts:12:4 - error TS2339: Property 'push' does not exist on type 'readonly number[]'.
+//
+// 12 as.push(6);
+// ~~~~
