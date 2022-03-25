fa4a09f785277847eb2a173cc883e4c8eae77784
tetsunari <mtst.tty556@gmail.com>
2022-03-25 21:10:36 +0900

    関数書き方

--- dist/index.js
+++ dist/index.js
@@ -1,28 +1,18 @@
 "use strict";
-let a = 1 /* English */;
-// let b = Language.Tagalog;
-// let c = Language[0];
-// let d = Language[8];
-console.log(a);
-// console.log(b);
-// console.log(c);
-// console.log(d);
-// error
-// src/index.ts:8:18 - error TS2339: Property 'Tagalog' does not exist on type 'typeof Language'.
-//
-// 8 let b = Language.Tagalog;
-// ~~~~~~~
-//
-//     src/index.ts:9:18 - error TS2476: A const enum member can only be accessed using a string literal.
-//
-// 9 let c = Language[0];
-// ~
-//
-//     src/index.ts:10:18 - error TS2476: A const enum member can only be accessed using a string literal.
-//
-// 10 let d = Language[8];
-// ~
-//
-//
-//     Found 3 errors in the same file, starting at: src/index.ts:8
+// 名前付き関数
+function greet(name) {
+    return 'hello' + name;
+}
+// 関数式
+function greet2(name) {
+    return 'hello' + name;
+}
+// アロー関数
+let greet3 = (name) => {
+    return 'hello' + name;
+};
+// アロー関数省略系
+let greet4 = (name) => 'hello' + name;
+greet('World');
+greet4('Tetsu');
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,20 @@
-const enum Language {
-    English = 1,
-    Spanish = 2,
-    Japanese = 3
+// 名前付き関数
+function greet(name: string) {
+    return 'hello' + name;
 }
 
-let a = Language.English;
-let b = Language.Tagalog;
-let c = Language[0];
-let d = Language[8];
+// 関数式
+function greet2(name: string) {
+    return 'hello' + name;
+}
 
-console.log(a); // 1
-console.log(b);
-console.log(c);
-console.log(d);
+// アロー関数
+let greet3 = (name: string) => {
+    return 'hello' + name;
+}
 
-// error
-// src/index.ts:8:18 - error TS2339: Property 'Tagalog' does not exist on type 'typeof Language'.
-//
-// 8 let b = Language.Tagalog;
-// ~~~~~~~
-//
-//     src/index.ts:9:18 - error TS2476: A const enum member can only be accessed using a string literal.
-//
-// 9 let c = Language[0];
-// ~
-//
-//     src/index.ts:10:18 - error TS2476: A const enum member can only be accessed using a string literal.
-//
-// 10 let d = Language[8];
-// ~
-//
-//
-//     Found 3 errors in the same file, starting at: src/index.ts:8
+// アロー関数省略系
+let greet4 = (name: string) => 'hello' + name;
+
+greet('World');
+greet4('Tetsu');
