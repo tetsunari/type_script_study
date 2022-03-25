b92b0e2529ee0c863c398f647f18e217586008ed
tetsunari <mtst.tty556@gmail.com>
2022-03-25 19:56:11 +0900

    array

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,16 @@
 "use strict";
-// intersection
-let b = {
-    name: 'Domino',
-    barks: true,
-    purrs: true,
-    wags: false
-};
-console.log(b);
+let a = [1, 2, 3];
+var b = ['a', 'b'];
+let c = ['a'];
+let d = [1, 'a'];
+const e = [2, 'b'];
+let f = ['red'];
+f.push('blue');
+f.push(true);
+let g = [];
+g.push(1);
+g.push(true);
+let h = [];
+h.push(1);
+h.push('blue');
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,28 @@
-type Cat = {name: string, purrs: boolean};
-type Dog = {name: string, barks: boolean, wags: boolean};
-type CatOrDogOrBoth = Cat | Dog;
-type CatAndDpg = Cat & Dog;
+let a = [1, 2, 3];
+var b = ['a', 'b'];
+let c: string[] = ['a'];
+let d = [1, 'a'];
+const e = [2, 'b'];
 
-// intersection
-let b: CatAndDpg = {
-    name: 'Domino',
-    barks: true,
-    purrs: true,
-    wags: false
-}
-console.log(b); // { name: 'Domino', barks: true, purrs: true, wags: false }
+let f = ['red'];
+f.push('blue');
+f.push(true);
+
+let g = [];
+g.push(1);
+g.push(true);
+
+let h: number[] = [];
+h.push(1);
+h.push('blue');
+
+// error
+// src/index.ts:9:8 - error TS2345: Argument of type 'boolean' is not assignable to parameter of type 'string'.
+//
+// 9 f.push(true);
+// ~~~~
+//
+//     src/index.ts:17:8 - error TS2345: Argument of type 'string' is not assignable to parameter of type 'number'.
+//
+// 17 h.push('blue');
+// ~~~~~~
