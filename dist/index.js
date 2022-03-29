1a2f59507c755b4263485c41710431c8da040523
tetsunari <mtst.tty556@gmail.com>
2022-03-29 19:54:10 +0900

    レストパラメータ

--- dist/index.js
+++ dist/index.js
@@ -1,22 +1,12 @@
 "use strict";
-function log(message, userId) {
-    const time = new Date().toLocaleTimeString();
-    console.log(time, message, userId || 'Not signed in');
+function sum(numbers) {
+    return numbers.reduce((total, n) => total + n, 0);
 }
-log('Page loaded');
-log('User signed in', 'da763be');
-// オプションパラメータ
-function log_2(message2, userId2 = 'Not singed in') {
-    const time2 = new Date().toISOString();
-    console.log(time2, message2, userId2);
+const test = sum([1, 2, 3]);
+console.log(test); // 6
+function sumVariadicSafe(...numbers) {
+    return numbers.reduce((toral, n) => toral + n, 0);
 }
-log_2('User clicked on a button', 'da763e');
-log_2('User signed out');
-function log_3(message3, context = {}) {
-    const time3 = new Date().toISOString();
-    console.log(time3, message3, context.userId);
-}
-log_3('test', { appId: 'test' });
-log_3('test', { userId: 'testId' });
-log_3('test');
+const betterTest = sumVariadicSafe(1, 2, 3, 4, 5);
+console.log(betterTest);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,12 @@
-function log(message: string, userId?: string) {
-    const time = new Date().toLocaleTimeString();
-    console.log(time, message, userId || 'Not signed in');
+function sum(numbers: number[]): number {
+    return numbers.reduce((total, n) => total + n, 0);
 }
 
-log('Page loaded'); // 21:28:50 Page loaded Not signed in
-log('User signed in', 'da763be'); // 21:28:50 User signed in da763be
-// オプションパラメータ
+const test = sum([1,2,3])
+console.log(test); // 6
 
-function log_2(message2: string, userId2 = 'Not singed in') {
-    const time2 = new Date().toISOString();
-    console.log(time2, message2, userId2);
+function sumVariadicSafe(...numbers: number[]): number {
+    return numbers.reduce((toral, n) => toral + n, 0);
 }
-log_2('User clicked on a button', 'da763e'); // 2022-03-25T12:28:50.566Z User clicked on a button da763e
-log_2('User signed out'); // 2022-03-25T12:28:50.567Z User signed out Not singed in
-// デフォルトパラメータ better
-
-type Context = {
-    appId?: string,
-    userId?: string
-}
-function log_3(message3: string, context: Context = {}) {
-    const time3 = new Date().toISOString();
-    console.log(time3, message3, context.userId);
-}
-log_3('test', {appId: 'test'}); // 2022-03-25T12:28:50.567Z test undefined
-log_3('test', {userId: 'testId'}); // 2022-03-25T12:28:50.567Z test testId
-log_3('test'); // 2022-03-25T12:28:50.567Z test undefined
+const betterTest = sumVariadicSafe(1,2,3,4,5);
+console.log(betterTest); // 15
